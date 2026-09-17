64d6f09d8f56acf7cfcbe729c9a8c93ebda1714a
sreyaharidas <151733059+sreyaharidas@users.noreply.github.com>
2026-09-17 12:53:35 +0530

    Improve input handling and validation in house_price.py
    Refactor input fields and validation for house price prediction.

--- house_price.py
+++ house_price.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import joblib
-model=joblib.load("house_price_model.pkl")
-st.title("Predicted price")
-area = st.number_input("Enter Area (sq ft)",min_value=0.0,max_value=5000.0,value=500.0)
 
+model = joblib.load("house_price_model.pkl")
 
-bedroom = st.number_input("Enter Number of Bedrooms",min_value=1,max_value=10,value=2)
+st.title("Predicted Price")
+
+area = st.number_input(
+    "Enter Area (sq ft)",
+    min_value=0.0,
+    max_value=5000.0,
+    value=500.0
+)
+
+bedroom = st.number_input(
+    "Enter Number of Bedrooms",
+    min_value=1,
+    max_value=10,
+    value=2
+)
 
 age = st.number_input(
     "Enter House Age (years)",
@@ -14,11 +26,20 @@ age = st.number_input(
     value=10.0
 )
 
+# Validation
 if area > 600:
     st.error("Area should not be more than 600 sq ft!!")
 
+elif area < 100:
+    st.error("Area should not be less than 100 sq ft!!")
+
+elif bedroom < 1 or bedroom > 5:
+    st.error("Bedrooms should not be less than 1 or more than 5!!")
+
+elif age < 1 or age > 4:
+    st.error("Age should not be less than 1 or more than 4!!")
+
 else:
-    
     if st.button("Predict Price"):
 
         input_data = [[area, bedroom, age]]
@@ -26,4 +47,7 @@ else:
         prediction = model.predict(input_data)
 
         st.subheader("Prediction Result")
-        st.success(f"Predicted House Price: ₹{prediction[0]:,.2f}")
+
+        st.success(
+            f"Predicted House Price: ₹{prediction[0]:,.2f}"
+        )
