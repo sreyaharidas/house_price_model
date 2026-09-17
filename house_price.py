c624da748baab8f448fed8cc6616527acee877aa
sreyaharidas <151733059+sreyaharidas@users.noreply.github.com>
2026-09-17 12:37:22 +0530

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import joblib
+model=joblib.load("house_price_model.pkl")
+st.title("Predicted price")
+area = st.number_input("Enter Area (sq ft)",min_value=0.0,max_value=5000.0,value=500.0)
 
+
+bedroom = st.number_input("Enter Number of Bedrooms",min_value=1,max_value=10,value=2)
+
+age = st.number_input(
+    "Enter House Age (years)",
+    min_value=0.0,
+    max_value=100.0,
+    value=10.0
+)
+
+if area > 600:
+    st.error("Area should not be more than 600 sq ft!!")
+
+else:
+    
+    if st.button("Predict Price"):
+
+        input_data = [[area, bedroom, age]]
+
+        prediction = model.predict(input_data)
+
+        st.subheader("Prediction Result")
+        st.success(f"Predicted House Price: ₹{prediction[0]:,.2f}")
